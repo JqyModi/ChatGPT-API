0ef2323c4dbb52132eaa79929cd813b54439eb50
JqyModi <jqy.tieniu@gmail.com>
2023-07-18 20:56:11 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express, { json, urlencoded } from 'express';
 import { completions, chatCompletions } from './routes.js';
 import { corsMiddleware, rateLimitMiddleware } from './middlewares.js';
 import { DEBUG, SERVER_PORT } from './config.js';
+import https from 'https';
+import fs from 'fs';
 
 let app = express();
 
@@ -27,7 +29,14 @@ app.all("/", async function (req, res) {
 app.post("/v1/completions", completions);
 app.post("/v1/chat/completions", chatCompletions);
 
-// Start server
-app.listen(SERVER_PORT, () => {
-    console.log(`Listening on ${SERVER_PORT} ...`);
+// SSL 证书和私钥路径
+const sslPath = '/etc/letsencrypt/live/802301.xyz/';
+const options = {
+    cert: fs.readFileSync(`${sslPath}fullchain.pem`),
+    key: fs.readFileSync(`${sslPath}privkey.pem`)
+};
+
+// 创建 HTTPS 服务器
+https.createServer(options, app).listen(SERVER_PORT, () => {
+    console.log(`Listening on ${SERVER_PORT} (HTTPS) ...`);
 });
